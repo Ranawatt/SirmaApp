36e1f0f35a249a40307b106ec33b26a812cc0243
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-04-18 19:08:12 +0530

    Added all the Layout file

---
+++ sirmaconsult/src/main/java/com/example/sirmaconsult/data/source/local/ToDoDatabase.kt
@@ -0,0 +1,16 @@
+package com.example.sirmaconsult.data.source.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.sirmaconsult.data.Task
+
+/**
+ * The Room Database that contains the Task table.
+ *
+ * Note that exportSchema should be true in production databases.
+ */
+@Database(entities = [Task::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TasksDao
+}
