a404746ea8ebf442e26c761c9d3a6d58eb619c71
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-04-25 17:59:25 +0530

    Created TasksLocalDataSourceTest to test the database method using room

---
+++ sirmaconsult/src/androidTest/java/com/example/sirmaconsult/data/source/local/TasksLocalDataSourceTest.kt
@@ -0,0 +1,91 @@
+package com.example.sirmaconsult.data.source.local
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.example.sirmaconsult.data.Result
+import com.example.sirmaconsult.data.Task
+import com.example.sirmaconsult.data.succeeded
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+class TasksLocalDataSourceTest {
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var localDataSource: TasksLocalDataSource
+    private lateinit var database: ToDoDatabase
+
+    @Before
+    fun setup() {
+        // Using an in-memory database for testing, because it doesn't survive killing the process.
+        database = Room.inMemoryDatabaseBuilder(
+                ApplicationProvider.getApplicationContext(),
+                ToDoDatabase::class.java
+        )
+                .allowMainThreadQueries()
+                .build()
+
+        localDataSource =
+                TasksLocalDataSource(
+                        database.taskDao(),
+                        Dispatchers.Main
+                )
+    }
+
+    @After
+    fun cleanUp() {
+        database.close()
+    }
+
+    // runBlocking is used here because of https://github.com/Kotlin/kotlinx.coroutines/issues/1204
+    // TODO: Replace with runBlockingTest once issue is resolved
+    @Test
+    fun saveTask_retrievesTask() = runBlocking {
+        // GIVEN - A new task saved in the database.
+        val newTask = Task("title", "description", false)
+        localDataSource.saveTask(newTask)
+
+        // WHEN  - Task retrieved by ID.
+        val result = localDataSource.getTask(newTask.id)
+
+        // THEN - Same task is returned.
+        assertThat(result.succeeded, `is`(true))
+        result as Result.Success
+        assertThat(result.data.title, `is`("title"))
+        assertThat(result.data.description, `is`("description"))
+        assertThat(result.data.isCompleted, `is`(false))
+    }
+
+    @Test
+    fun completeTask_retrievedTaskIsComplete() = runBlocking {
+        // 1. Save a new active task in the local data source.
+        val newTask = Task("newTitle")
+        localDataSource.saveTask(newTask)
+        // When completed in the persistent repository
+        localDataSource.completeTask(newTask)
+        val result = localDataSource.getTask(newTask.id)
+
+        // Then the task can be retrieved from the persistent repository and is complete
+        assertThat(result.succeeded, `is`(true))
+        result as Result.Success
+        assertThat(result.data.title, `is`(newTask.title))
+        assertThat(result.data.isCompleted, `is`(true))
+
+    }
+}
